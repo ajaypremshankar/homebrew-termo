a4ed7697c13a861249c4c3ff2f6236ea711be835
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-11-12 22:18:06 +0530

    Updated version release in micro-cli

--- macro-cli.rb
+++ macro-cli.rb
@@ -3,8 +3,8 @@ class MacroCli < Formula
   
     desc "A CLI tool for recording and running macros in the terminal"
     homepage "https://github.com/ajaypremshankar/macro-cli"  # replace with your repo URL
-    url "https://github.com/ajaypremshankar/macro-cli/releases/download/1.0.0/macro_cli-1.0.tar.gz"  # replace with the URL of your .tar.gz
-    sha256 "a56e66b267497c1abfc58b27fc399f7df20de6abe318145b9ed2d04f04b37a54"  # replace with the SHA256 hash
+    url "https://github.com/ajaypremshankar/homebrew-macro-cli/releases/download/1.0.1/macro_cli-1.0.1.tar.gz"  # replace with the URL of your .tar.gz
+    sha256 "fea7bbc9becf5ed1ff45dfb05e83afb6636e4a6aa509da7e9948069518cabbfc"  # replace with the SHA256 hash
     license "MIT"
   
     depends_on "python@3.12"  # Specify Python version if needed
