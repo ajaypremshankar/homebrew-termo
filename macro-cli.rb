76e385bed662c8b612b4b343fbddd993bfca4dc2
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-11-12 22:21:07 +0530

    Updated version release in micro-cli

--- macro-cli.rb
+++ macro-cli.rb
@@ -2,12 +2,12 @@ class MacroCli < Formula
     include Language::Python::Virtualenv
   
     desc "A CLI tool for recording and running macros in the terminal"
-    homepage "https://github.com/ajaypremshankar/macro-cli"  # replace with your repo URL
-    url "https://github.com/ajaypremshankar/homebrew-macro-cli/releases/download/1.0.1/macro_cli-1.0.1.tar.gz"  # replace with the URL of your .tar.gz
-    sha256 "fea7bbc9becf5ed1ff45dfb05e83afb6636e4a6aa509da7e9948069518cabbfc"  # replace with the SHA256 hash
+    homepage "https://github.com/ajaypremshankar/macro-cli"
+    url "https://github.com/ajaypremshankar/homebrew-macro-cli/releases/download/1.0.1/macro_cli-1.0.1.tar.gz"
+    sha256 "fea7bbc9becf5ed1ff45dfb05e83afb6636e4a6aa509da7e9948069518cabbfc"  
     license "MIT"
   
-    depends_on "python@3.12"  # Specify Python version if needed
+    depends_on "python@3.12"
   
     def install
       virtualenv_install_with_resources
