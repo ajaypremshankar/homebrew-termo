e3d9aa2a9886ffd76ef25ebac8af39f718964594
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-11-12 22:45:34 +0530

    Updated version

--- macro-cli.rb
+++ macro-cli.rb
@@ -10,11 +10,9 @@ class MacroCli < Formula
     depends_on "python@3.12"
 
     # Resource for click, only for Python 3.12 and above
-    if Formula["python@3.10"].version >= "3.12"
-        resource "click" do
-        url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
-        sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
-        end
+    resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
     end
   
     def install
